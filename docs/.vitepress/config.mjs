55229431bdeb50f35f9a356b67d119140d80deed
Sirius Leaf <hfzamdhai@gmail.com>
2026-07-19 07:08:33 +0700

    tambah tailwind

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vitepress'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Hafiz Ahmad Husaini",
-  description: "Portfolio"
+	title: "Hafiz Ahmad Husaini",
+	description: "Portfolio",
+	
+	vite: {
+		plugins: [tailwindcss()],
+	},
 })
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,7 @@
 ---
-home: true
+home: false
 ---
 
+<p class="italic text-blue">
+    Ini paragraf yang di-style pakai Tailwind langsung di markdown.
+</p>
